cb570c35d6306bbc5367c7136075dc37782f4e31
Jayatri Sahoo <sahoojayatri2011@gmail.com>
2026-08-05 10:44:19 +0530

    Create 05. Multiplication Table.py

---
+++ 05. Multiplication Table.py
@@ -0,0 +1,22 @@
+5. Multiplication Table
+Program
+num = int(input("Enter a number: "))
+for i in range(1, 11):
+print(num, "x", i, "=", num * i)
+
+Sample Input
+Enter a number: 5
+Output
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50
+
+Explanation
+Uses a for loop to print the multiplication table from 1 to 10.
