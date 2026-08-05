73e636513a5b93d7abbe38063326f791f94517ce
Jayatri Sahoo <sahoojayatri2011@gmail.com>
2026-08-05 10:41:24 +0530

    Create 02. Add Two Numbers.py

---
+++ 02. Add Two Numbers.py
@@ -0,0 +1,20 @@
+2. Add Two Numbers
+Program
+--------
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1 + num2
+print("Sum =", sum)
+
+Sample Input
+-------------
+Enter first number: 10
+Enter second number: 20
+Output
+Sum = 30
+
+Explanation
+------------
+Takes two numbers as input.
+Adds them using the + operator.
+Prints the result.
