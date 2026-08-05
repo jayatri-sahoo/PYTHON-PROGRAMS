f23eb67ab7e1189680ca410bd88be428daa756db
Jayatri Sahoo <sahoojayatri2011@gmail.com>
2026-08-05 10:42:11 +0530

    Create 3. Check Even or Odd.py

---
+++ 3. Check Even or Odd.py
@@ -0,0 +1,17 @@
+3. Check Even or Odd
+Program
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
+
+Sample Input
+Enter a number: 15
+Output
+Odd Number
+
+Explanation
+If the remainder after dividing by 2 is 0, the number is even.
+Otherwise, it is odd.
