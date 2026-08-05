43da4dd1605c9325cb3ab83e9f38729d6ed1f489
Jayatri Sahoo <sahoojayatri2011@gmail.com>
2026-08-05 10:43:10 +0530

    Create 04. Find the Largest of Three Numbers.py

---
+++ 04. Find the Largest of Three Numbers.py
@@ -0,0 +1,17 @@
+4. Find the Largest of Three Numbers
+Program
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+largest = max(a, b, c)
+print("Largest Number =", largest)
+
+Sample Input
+Enter first number: 45
+Enter second number: 87
+Enter third number: 62
+Output
+Largest Number = 87
+
+Explanation
+max() returns the largest value among the given numbers.
