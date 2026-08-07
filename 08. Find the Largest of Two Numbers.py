7b306d365748d0140d316b659b1a80cb8517e1eb
Jayatri Sahoo <sahoojayatri2011@gmail.com>
2026-08-07 21:52:59 +0530

    Create 08. Find the Largest of Two Numbers.py

---
+++ 08. Find the Largest of Two Numbers.py
@@ -0,0 +1,22 @@
+08. Find the Largest of Two Numbers
+Python
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("Largest Number:", a)
+else:
+    print("Largest Number:", b)
+Explanation
+The program reads two numbers.
+
+It compares them using >.
+
+The larger number is displayed.
+
+Sample Output
+
+Enter first number: 25
+Enter second number: 18
+Largest Number: 25
