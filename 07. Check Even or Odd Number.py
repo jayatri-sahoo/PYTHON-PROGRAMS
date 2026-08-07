a166e5ee2d9c6ffd45dc4fc8c02d5febc9b23d12
Jayatri Sahoo <sahoojayatri2011@gmail.com>
2026-08-07 21:52:32 +0530

    Create 07. Check Even or Odd Number.py

---
+++ 07. Check Even or Odd Number.py
@@ -0,0 +1,20 @@
+07. Check Even or Odd Number
+Python
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
+Explanation
+The user enters a number.
+
+% gives the remainder after division.
+
+If the remainder is 0, the number is even; otherwise, it is odd.
+
+Sample Output
+
+Enter a number: 8
+Even Number
