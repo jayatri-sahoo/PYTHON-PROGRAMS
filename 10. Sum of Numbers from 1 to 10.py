07d2931b765183917ea0579da581e54e51136eaf
Jayatri Sahoo <sahoojayatri2011@gmail.com>
2026-08-07 21:54:00 +0530

    Create 10. Sum of Numbers from 1 to 10.py

---
+++ 10. Sum of Numbers from 1 to 10.py
@@ -0,0 +1,19 @@
+10. Sum of Numbers from 1 to 10
+Python
+
+total = 0
+
+for i in range(1, 11):
+    total = total + i
+
+print("Sum =", total)
+Explanation
+total starts at 0.
+
+The loop adds numbers from 1 to 10.
+
+The final sum is printed.
+
+Output
+
+Sum = 55
