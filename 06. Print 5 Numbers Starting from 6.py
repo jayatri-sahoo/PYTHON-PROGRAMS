b751f199da4b1864ec8f627676b2bb5decd81d41
Jayatri Sahoo <sahoojayatri2011@gmail.com>
2026-08-07 21:51:41 +0530

    Create 06. Print 5 Numbers Starting from 6.py

---
+++ 06. Print 5 Numbers Starting from 6.py
@@ -0,0 +1,19 @@
+06. Print 5 Numbers Starting from 6
+Python
+
+for i in range(6, 11):
+    print(i)
+Explanation:
+
+range(6, 11) generates numbers from 6 to 10.
+
+The for loop prints each number one by one.
+
+Output:
+
+
+6
+7
+8
+9
+10
