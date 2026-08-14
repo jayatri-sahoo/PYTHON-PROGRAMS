70c6b85a50d824f8dee887eba62f60da306dd201
Jayatri Sahoo <sahoojayatri2011@gmail.com>
2026-08-14 23:09:00 +0530

    Create 23. Find the Sum of All Elements in a List.py

---
+++ 23. Find the Sum of All Elements in a List.py
@@ -0,0 +1,16 @@
+23. Find the Sum of All Elements in a List
+numbers = [10, 20, 30, 40, 50]
+
+
+total = 0
+
+
+for num in numbers:
+    total += num
+
+
+print("Sum of elements:", total)
+
+Output:
+
+Sum of elements: 150
