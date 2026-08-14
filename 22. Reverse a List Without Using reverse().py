b77d93e56fc7c6d47b09af49dbd498f169c6182b
Jayatri Sahoo <sahoojayatri2011@gmail.com>
2026-08-14 23:08:36 +0530

    Create 22. Reverse a List Without Using reverse().py

---
+++ 22. Reverse a List Without Using reverse().py
@@ -0,0 +1,14 @@
+22. Reverse a List Without Using reverse()
+numbers = [10, 20, 30, 40, 50]
+
+
+reversed_list = numbers[::-1]
+
+
+print("Original list:", numbers)
+print("Reversed list:", reversed_list)
+
+Output:
+
+Original list: [10, 20, 30, 40, 50]
+Reversed list: [50, 40, 30, 20, 10]
